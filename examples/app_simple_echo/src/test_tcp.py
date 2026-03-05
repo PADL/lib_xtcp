ae9307d1b2188dd0cf6ccad27ce7aaa041b4bb04
Humphrey Bucknell <humphreybucknell@xmos.com>
2026-03-05 09:25:03 +0000

    Updating project files

--- examples/app_simple_echo/src/test_tcp.py
+++ examples/app_simple_echo/src/test_tcp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright 2025 XMOS LIMITED.
+# Copyright 2025-2026 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 import argparse
